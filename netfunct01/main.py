1bfd6450c7e72f6f3bb3d91478a45f9201179a45
Bruce B. A. West <brucebawest@gmail.com>
2020-06-30 18:06:53 -0400

    done for the day cuz my brain is fried

---
+++ netfunct01/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Author: Bruce West
+"""
+
+# function to push commands
+def commandpush(devicecmd): # devicecmd==list
+    for coffeetime in devicecmd.keys():
+        print('Handshaking. .. ... connecting with ' + coffeetime )
+    
+        for mycmds in devicecmd[coffeetime]:
+            print('Attempting to send command --> ' + mycmds + '\n')
+            # we'll learn to write code that sends cmds to device here
+
+# second function iterates through list ip's
+# prints "connection to.." then "REBOOTING NOW" 
+# def devicereboot
+
+# start our main script
+def main():
+    work2do = {"10.1.0.1":["interface eth1/2", "no shutdown"], "10.2.0.1": 
+    ["interface eth1/1", "shutdown"], "10.3.0.1":["interface eth1/5", "no shutdown"]} 
+    # data that replaces data stored in file
+
+    print("Welcome to the network device command pusher") # welcome message
+
+    ## get data set
+    print("\nData set found\n") # replace with function call that reads in data from file
+
+    ## run
+    commandpush(work2do) # call function to push commands to devices
+
+# call our main function
+main()
